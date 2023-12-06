2d2fc117f6b23e5aec77cd9368db046b48409d9c
Vittorio Lo Preiato <36620514+falconeta@users.noreply.github.com>
2023-12-06 19:17:27 +0100

    fix unfill

--- packages/svg-icons/unfill.js
+++ packages/svg-icons/unfill.js
@@ -8,7 +8,7 @@ const { parseArgs } = require('node:util');
 const { values } = parseArgs({ options: { path: { type: 'string' } } });
 
 replaceInFiles({
-  search: /\sfill="(none|#212121|#111827|#6B7280|#242424|#EE0000)"/g,
+  search: /\sfill="(none|#212121|#111827|#6B7280|#242424|#EE0000|#E00)"/g,
   replacement: '',
   paths: [values.path],
   recursive: true,
